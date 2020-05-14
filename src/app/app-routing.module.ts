27a16a123b15638d0b850365144a10ec927d5a66
jacobpineault <jacob.pineault5@gmail.com>
2020-05-14 17:34:50 -0400

    user entity completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserListComponent } from './feature/user/user-list/user-list.component';
 import { UserDetailComponent } from './feature/user/user-detail/user-detail.component';
+import { UserCreateComponent } from './feature/user/user-create/user-create.component';
+import { UserEditComponent } from './feature/user/user-edit/user-edit.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: UserListComponent },
   { path: 'user/list', component: UserListComponent },
   { path: 'user/detail/:id', component: UserDetailComponent },
+  { path: 'user/create', component: UserCreateComponent },
+  { path: 'user/edit/:id', component: UserEditComponent },
   { path: '**', component: UserListComponent },
 ];
 
