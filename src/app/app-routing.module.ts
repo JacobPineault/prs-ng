c39ae47dc9b9ce53ec7a9f5dd696397a09ef2d22
jacobpineault <jacob.pineault5@gmail.com>
2020-05-14 16:41:09 -0400

    user detail implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserListComponent } from './feature/user/user-list/user-list.component';
+import { UserDetailComponent } from './feature/user/user-detail/user-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: UserListComponent },
   { path: 'user/list', component: UserListComponent },
+  { path: 'user/detail/:id', component: UserDetailComponent },
   { path: '**', component: UserListComponent },
 ];
 
--- src/app/feature/user/user-create/user-create.component.ts
+++ src/app/feature/user/user-create/user-create.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/model/user.class';
 
 @Component({
   selector: 'app-user-create',
-  templateUrl: './user-create.component.html',
-  styleUrls: ['./user-create.component.css']
+  templateUrl: '../actor-maint-shared/actor-maint.component.html',
+  styleUrls: ['./user-create.component.css'],
 })
 export class UserCreateComponent implements OnInit {
+  title: string = 'Actor Create';
+  user: User = new User();
+  submitBtnTitle: string = 'Create';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
